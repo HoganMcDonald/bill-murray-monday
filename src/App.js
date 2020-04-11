4a7b0c2394afb80cf1828c49d9bfbba11eaf0849
Hogan McDonald <hogan.developer@gmail.com>
2020-04-11 14:53:52 -0500

    loads toggler from state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Toggle from "react-toggle";
+import "./App.css";
+import "react-toggle/style.css";
 
 function App() {
+  const [on, setOn] = React.useState(false);
+
+  const handleToggle = React.useCallback(() => {
+    window.chrome.storage.local.set({ bill_murray_monday: !on });
+    setOn(!on);
+  }, [on]);
+
+  React.useEffect(() => {
+    window.chrome.storage.local.get("bill_murray_monday", (res) => {
+      setOn(!!res.bill_murray_monday);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="title">Bill Murray Monday</p>
+      <Toggle checked={on} onChange={handleToggle} />
     </div>
   );
 }
