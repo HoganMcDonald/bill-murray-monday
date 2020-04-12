ef879d64b280b648180a9c3130c6f871267fa4dd
Hogan McDonald <hogan.developer@gmail.com>
2020-04-11 20:58:03 -0500

    sends message to content script when toggled

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import "react-toggle/style.css";
 
 function App() {
   const [on, setOn] = React.useState(false);
-
-  const handleToggle = React.useCallback(() => {
-    window.chrome.storage.local.set({ bill_murray_monday: !on });
-    setOn(!on);
-  }, [on]);
+  const [tabId, setTabId] = React.useState(null);
 
   React.useEffect(() => {
-    window.chrome.storage.local.get("bill_murray_monday", (res) => {
-      setOn(!!res.bill_murray_monday);
+    window.chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      setTabId(tabs[0].id);
     });
   }, []);
 
+  const notifyPage = (value) => {
+    window.chrome.tabs.sendMessage(tabId, value ? "on" : "off");
+  };
+
+  const handleToggle = React.useCallback(() => {
+    notifyPage(!on);
+    setOn(!on);
+  }, [on, tabId]);
+
   return (
     <div className="App">
-      <p className="title">Bill Murray Monday</p>
       <Toggle checked={on} onChange={handleToggle} />
     </div>
   );
